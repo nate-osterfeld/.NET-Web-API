2ac9bae364105fefb2a2b35aa02e32287d357e83
Nate Osterfeld <osterfnl@mail.uc.edu>
2025-07-15 20:29:47 -0400

    (http) GET /api/name

---
+++ M10_Git/Controllers/NameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace M10_Git.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NameController : ControllerBase
+    {
+        // GET: api/<NameController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new
+            {
+                name = "Nate Osterfeld"
+            };
+
+            return Ok(result);
+        }
+    }
+}
